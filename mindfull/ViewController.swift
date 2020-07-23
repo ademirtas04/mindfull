93b39a8666e535de566f8d5d17e8acfd117ca6d8
Alp Demirtas <ademirtas@alps-mac.local>
2020-07-23 08:43:41 -0700

    Initial Commit

---
+++ mindfull/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mindfull
+//
+//  Created by Alp Demirtas on 7/23/20.
+//  Copyright © 2020 Alp Demirtas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
