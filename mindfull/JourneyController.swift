2cb2f6c59f42c049d742df48e5b8be747b265698
Alp Demirtas <ademirtas@alps-mac.local>
2020-07-28 08:40:38 -0700

    journey begun

--- mindfull/JourneyController.swift
+++ mindfull/JourneyController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class JourneyController: UIViewController {
     var coins: Int = 0
+    var completed: Int = 0
     let numStops: Int = 15
+    
+    
 
+    @IBOutlet weak var Header: UILabel!
+    @IBOutlet weak var Description: UILabel!
     var journey = [JourneyStop]()
     
 
@@ -24,4 +29,68 @@ class JourneyController: UIViewController {
     func addCoins(c: Int){
         coins = coins + c
     }
+    
+    
+    @IBAction func stopOnePressed(_ sender: Any) {
+        Header.text = "Your First Mindfulness Exercise"
+        Description.text = "Meditate for 30 seconds"
+    }
+    
+    
+    @IBAction func stopTwoPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopThreePressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopFourPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopFivePressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopSixPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopSevenPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopEightPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopNinePressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopTenPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopElevenPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopTwelvePressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopThirteenPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopFourteenPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func stopFifteenPresssed(_ sender: Any) {
+    }
+    
+    
 }
