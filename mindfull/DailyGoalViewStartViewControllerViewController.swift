b996f39d9e9db9da4c08ffccc7f2df22745f6b41
Tyler Turek <tylerturek5804@gmail.com>
2020-08-03 17:49:13 -0500

    DWG Home Screen and DWG Goal View

---
+++ mindfull/DailyGoalViewStartViewControllerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DailyGoalViewStartViewControllerViewController.swift
+//  mindfull
+//
+//  Created by Tyler Turek on 8/3/20.
+//  Copyright © 2020 Alp Demirtas. All rights reserved.
+//
+
+import UIKit
+
+class DailyGoalViewStartViewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //Switching to DWG home
+    @IBAction func toDWGHomeScreen(_ sender: Any) {
+        self.performSegue(withIdentifier: "toDWGHome", sender: self)
+    }
+}
