00482d2bd8eb598fbce67ae52df186d1fc2bb8b1
Alp Demirtas <a.demirtas04@yahoo.com>
2020-08-06 13:36:39 -0500

    beginning work on journal

--- mindfull/JournalController.swift
+++ mindfull/JournalController.swift
@@ -7,27 +7,55 @@
 //
 
 import UIKit
-
 class JournalController: UIViewController {
 
-    var journalText: String = ""
+    var journalList = [Entry]()
+    var currentDate: Date = Date()
+    
+    @IBOutlet weak var dateTitle: UILabel!
+    
     @IBOutlet weak var journalField: UITextView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        journalField.text = journalText
-
+        
         // Do any additional setup after loading the view.
     }
     
-    func addLine(line: String){
-        journalText = journalText + "\n" + line
-        journalField.text = journalText
+    override func viewWillAppear(_ animated: Bool) {
+        if(journalList.count != 0){
+            let date: Date = journalList[0].date
+            currentDate = date
+            let calendar = Calendar.current
+            let dd = calendar.component(.day, from: date)
+            let MM = calendar.component(.month, from: date)
+            let yyyy = calendar.component(.year, from: date)
+            dateTitle.text = "\(dd)/\(MM)/\(yyyy)"
+            
+            for entry in journalList {
+                let d: Date = entry.date
+                let calendar = Calendar.current
+                let DD = calendar.component(.day, from: d)
+                let mm = calendar.component(.month, from: d)
+                let YYYY = calendar.component(.year, from: d)
+                if(dd == DD && MM == mm && yyyy == YYYY){
+                    let min = calendar.component(.minute, from: date)
+                    let hour = calendar.component(.hour, from: date)
+                    journalField.text = journalField.text + "\n" + "\(hour):\(min)" + "\n" + entry.prompt + "\n" + entry.body
+                }
+            }
+        }
     }
     
-    func addChunk(chunk: String) {
-        journalText = journalText + "\n" + chunk
-        journalField.text = journalText
+    @IBAction func leftPressed(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func rightPressed(_ sender: Any) {
+        
     }
     
     @IBAction func goToGarden(_ sender: Any) {
