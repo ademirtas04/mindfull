5aa396223051acb546a46c664591188683af41c6
Tyler Turek <tylerturek5804@gmail.com>
2020-08-14 16:15:20 -0500

    DWG type of goal

--- mindfull/WeeklyGoalViewOngoingViewController.swift
+++ mindfull/WeeklyGoalViewOngoingViewController.swift
@@ -52,7 +52,8 @@ class WeeklyGoalViewOngoingViewController: UIViewController {
     func setUpScreen() {
         WG2Title.text = thisGoal.getTitle()
         WG2Description.text = thisGoal.getDescription()
-        WG2Reward.text = "Goal Reward: +\(String(thisGoal.getxpPoints()))xp"
+        let theType = getType()
+        WG2Reward.text = "Goal Reward: +\(String(thisGoal.getxpPoints())) \(theType) xp"
         
         WG2AdditionalResources1.backgroundColor = UIColor.lightGray
         WG2AdditionalResources1.setTitle("None", for: .normal)
@@ -93,6 +94,25 @@ class WeeklyGoalViewOngoingViewController: UIViewController {
         else if thisGoal.getTypeOfGoal() == 4 {
             WG2AdditionalResources1.backgroundColor = UIColor.darkGray
             WG2AdditionalResources1.setTitle("Create a Journal Entry", for: .normal)
+        }
+    }
+    
+    //Getting the type of the goal (for xp)
+    func getType() -> String {
+        if thisGoal.getTypeOfGoal() == 1 {
+            return "Audio"
+        }
+        else if thisGoal.getTypeOfGoal() == 2 {
+            return "Wrriten"
+        }
+        else if thisGoal.getTypeOfGoal() == 3 {
+            return "Productivity"
+        }
+        else if thisGoal.getTypeOfGoal() == 4 {
+            return "Environment"
+        }
+        else if thisGoal.getTypeOfGoal() == 5 {
+            return "Assesment"
         }
     }
     
